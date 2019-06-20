e72594eacadd22cd1113f1c9e690f617c6931e4a
Mickey <poverwhelming@gmail.com>
2019-06-20 17:29:58 +0800

    implement html body scraping

--- lib/page_crawlr.ex
+++ lib/page_crawlr.ex
@@ -3,8 +3,23 @@ defmodule PageCrawlr do
   Documentation for PageCrawlr.
   """
 
+  # NOTE in practice, this would be set in config
+  @get_page if Mix.env == :test, do: PageCrawlr.GetPageMock, else: PageCrawlr.GetPage
+
   @doc """
   fetch image assets url and links from a page.
   """
   def fetch(""), do: %{assets: [], links: []}
+
+  def fetch(url) do
+    with {:ok, html} <- @get_page.get(url),
+         assets <- Floki.find(html, "img") |> Floki.attribute("src"),
+         links <- Floki.find(html, "a") |> Floki.attribute("href")
+    do
+      %{assets: assets, links: links}
+    else
+      {:error, msg} when is_integer(msg) ->
+        "Can't get url, http error: #{msg}"
+    end
+  end
 end
